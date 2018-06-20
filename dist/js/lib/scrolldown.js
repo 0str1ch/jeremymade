bbd1432a110be69d35f254680b76b1d84454adbf
0str1ch <get@smithed.xyz>
2018-06-20 00:24:05 -0400

    gfd

--- dist/js/lib/scrolldown.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function() {
-    if ($(window).scrollTop() >= 50) {
-  $(".scrolldown").css("visibility", "hidden");
-    } else {
-    $(".scrolldown").css("visibility", "initial");
-    }
-  });
